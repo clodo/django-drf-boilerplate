fe286ccfe341832b38adeb6ff90c43dab7b59805
Clodo <claudiobidau@gmail.com>
2019-05-28 16:23:31 -0300

    Add unit tests to login endpoint.

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# django-drf-boilerplate
+Django DRF Boilerplate
+############
+
+Boilerplate with Django, DRF and authentication. 
+
+Requirements
+============
+
+* Python >= 3.5
+* Django >= 2.1
+
+
+Installation
+============
+
+1. Create a .env file in root folder:
+
+* Use .env.template
+
+* Update variables according to your needs
+
+2. Ubuntu dependencies
+
+.. code-block:: bash
+
+    $ sudo apt install python3-dev
+
+2. Install (dev) requirements:
+
+.. code-block:: bash
+
+    $ pip install -r requirements/local.txt
+
+3. Run migrations:
+
+.. code-block:: bash
+
+    $ python manage.py migrate
+
+4. Create superuser
+
+.. code-block:: bash
+
+    $ python manage.py createsuperuser --email claudiobidau@gmail.com
+
+5. Run tests:
+
+.. code-block:: bash
+
+    $ python manage.py test --settings=config.settings.test
+
+6. Run server:
+
+.. code-block:: bash
+
+    $ python manage.py runserver
+
+7. Code coverage:
+
+.. code-block:: bash
+
+    $ coverage run --source='project/api' manage.py test --settings=config.settings.test
+
+    $ coverage report
--- project/api/tests.py
+++ project/api/tests.py
@@ -1,6 +1,53 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.reverse import reverse
+from rest_framework import status
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+factory = APIRequestFactory()
+request = factory.get('/')
+
+
+class ApiTest(APITestCase):
+
+    def setUp(self):
+        pass
+
+    def test_login_with_invalid_credentials(self):
+        payload = {
+            "email": "wrong@bika54.com",
+            "password": "P@ssw0rd"
+        }
+
+        response = self.client.post(reverse('rest_login'), data=payload)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data["non_field_errors"], ["Unable to log in with provided credentials."])
+
+    def test_login_with_valid_credentials_returns_token_and_user_data(self):
+        user = User.objects.create_user("clodo", "clodo@bika54.com", "P@ssw0rd")
+        user.first_name = "Claudio"
+        user.last_name = "Bidau"
+        user.save()
+
+        payload = {
+            "email": "clodo@bika54.com",
+            "password": "P@ssw0rd"
+        }
+
+        response = self.client.post(reverse('rest_login'), payload)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertIsNotNone(response.data["token"])
+        self.assertEqual(response.data["user"], {
+            "pk": 1,
+            "username": "clodo",
+            "email": "clodo@bika54.com",
+            "first_name": "Claudio",
+            "last_name": "Bidau"
+        })
+
 
 # api/v1/rest-auth/password/reset/
 # api/v1/rest-auth/password/reset/confirm/
